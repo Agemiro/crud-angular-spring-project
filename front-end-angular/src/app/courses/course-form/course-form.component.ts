03eda691b4fabe6a176899dc9811dc78ad61e02a
Agemiro Alves <agemiroalves11@gmail.com>
2023-03-26 21:37:29 -0300

    HTTP Post.

--- front-end-angular/src/app/courses/course-form/course-form.component.ts
+++ front-end-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,9 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -9,14 +13,39 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CourseFormComponent {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar,
+    private location: Location
+  ) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null],
     });
   }
 
-  onSumit() {}
+  onSumit() {
+    this.service.save(this.form.value).subscribe(
+      (result) => this.onSucess(),
+      (error) => this.onError()
+    );
+  }
 
-  onCancel() {}
+  private onError() {
+    this._snackBar.open('An error occurred while creating the course.', '', {
+      duration: 5000,
+    });
+  }
+
+  private onSucess() {
+    this._snackBar.open('Course created successfully.', '', {
+      duration: 5000,
+    });
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.location.back();
+  }
 }
